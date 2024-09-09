9008e1ac4723b699d1019e90bbd05b4a352c8282
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-09-09 15:24:01 +0000

    user files

---
+++ models/review.py
@@ -0,0 +1,17 @@
+ Review class
+"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Review class!
+
+    Attributes:
+        place_id (str): ID of place with review (init'd as empty string)
+        user_id (str): ID of user leaving review (init'd as empty string)
+        text (str): text of review (init'd as empty string)
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
